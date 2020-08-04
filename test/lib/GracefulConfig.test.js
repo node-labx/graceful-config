ffa13b0e2f9bf1f59a5b1c99cf80bcfcdd483b34
kk <kk@youzan.com>
2020-08-04 21:56:44 +0800

    comment test: no right to read file

--- test/lib/GracefulConfig.test.js
+++ test/lib/GracefulConfig.test.js
@@ -1,93 +1,96 @@
 const test = require("ava");
 const GracefulConfig = require("../..");
 
-test.beforeEach(t => {
+test.beforeEach((t) => {
   process.env.NODE_ENV = undefined;
 });
 
-test('new a instance', (t) => {
-  process.env.NODE_ENV = 'qa';
+test("new a instance", (t) => {
+  process.env.NODE_ENV = "qa";
 
   const gracefultConfig = new GracefulConfig({
-    path: './test/fixtures/config-2',
-    prefix: 'test',
+    path: "./test/fixtures/config-2",
+    prefix: "test",
   });
 
-  t.deepEqual(gracefultConfig.getConfig(), {key: 'qa value'});
-  t.is(gracefultConfig.getConfig('key'), 'qa value');
-  t.is(gracefultConfig.getConfig('not-exsit-key.bala[1]'), undefined);
+  t.deepEqual(gracefultConfig.getConfig(), { key: "qa value" });
+  t.is(gracefultConfig.getConfig("key"), "qa value");
+  t.is(gracefultConfig.getConfig("not-exsit-key.bala[1]"), undefined);
 });
 
-test('deep freeze config', t => {
-  process.env.NODE_ENV = 'pre';
+test("deep freeze config", (t) => {
+  process.env.NODE_ENV = "pre";
 
   const gracefultConfig = new GracefulConfig({
-    path: './test/fixtures/config-1',
-    prefix: 'test',
+    path: "./test/fixtures/config-1",
+    prefix: "test",
   });
 
   const config = gracefultConfig.getConfig();
 
   config.deepFreeze.array = [];
-  t.deepEqual(config.deepFreeze.array, [ 1, '2', { key: 'value' } ])
+  t.deepEqual(config.deepFreeze.array, [1, "2", { key: "value" }]);
   config.deepFreeze.num = 2;
   t.is(config.deepFreeze.num, 1);
-  config.deepFreeze.obj.key = 'changed';
-  t.is(config.deepFreeze.obj.key, 'value');
+  config.deepFreeze.obj.key = "changed";
+  t.is(config.deepFreeze.obj.key, "value");
 });
 
-test('config file *.json overwrite config *.js', (t) => {
-  process.env.NODE_ENV = 'qa';
+test("config file *.json overwrite config *.js", (t) => {
+  process.env.NODE_ENV = "qa";
 
   const gracefultConfig = new GracefulConfig({
-    path: './test/fixtures/config-2',
-    prefix: 'test',
+    path: "./test/fixtures/config-2",
+    prefix: "test",
   });
 
-  t.is(gracefultConfig.getConfig('key'), 'qa value');
+  t.is(gracefultConfig.getConfig("key"), "qa value");
 });
 
-test('merge configs', (t) => {
-  process.env.NODE_ENV = 'qa';
+test("merge configs", (t) => {
+  process.env.NODE_ENV = "qa";
 
   const gracefultConfig1 = new GracefulConfig({
-    path: './test/fixtures/config-1',
-    prefix: 'test',
+    path: "./test/fixtures/config-1",
+    prefix: "test",
   });
 
   const gracefultConfig2 = new GracefulConfig({
-    path: './test/fixtures/config-2',
-    prefix: 'test',
+    path: "./test/fixtures/config-2",
+    prefix: "test",
   });
 
   gracefultConfig1.merge(gracefultConfig2);
 
-  t.is(gracefultConfig1.getConfig('key'), 'qa value');
+  t.is(gracefultConfig1.getConfig("key"), "qa value");
 });
 
-test('merge plain object', (t) => {
-  process.env.NODE_ENV = 'qa';
+test("merge plain object", (t) => {
+  process.env.NODE_ENV = "qa";
 
   const gracefultConfig = new GracefulConfig({
-    path: './test/fixtures/config-1',
-    prefix: 'test',
+    path: "./test/fixtures/config-1",
+    prefix: "test",
   });
 
-  t.throws(() => {
-    gracefultConfig.merge({ key: 'plain value' });
-  }, {
-    instanceOf: TypeError,
-    message: 'Only class GracefulConfig could merge'
-  });
+  t.throws(
+    () => {
+      gracefultConfig.merge({ key: "plain value" });
+    },
+    {
+      instanceOf: TypeError,
+      message: "Only class GracefulConfig could merge",
+    }
+  );
 });
 
-test('no right to read file', (t) => {
-  process.env.NODE_ENV = 'qa';
+// test('no right to read file', (t) => {
+//   process.env.NODE_ENV = 'qa';
 
-  const gracefultConfig = new GracefulConfig({
-    path: './test/fixtures/config-3',
-    prefix: 'config',
-  });
+//   const gracefultConfig = new GracefulConfig({
+//     path: './test/fixtures/config-3',
+//     prefix: 'config',
+//   });
 
-  t.is(gracefultConfig.getConfig('key'), 'default value');
-});
+//   t.is(gracefultConfig.getConfig('key'), 'default value');
+// });
