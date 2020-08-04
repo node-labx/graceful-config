c73d849f0395131aaf4f557f9d6a859618420079
yidafu <yidafu@youzan.com>
2020-08-04 18:59:57 +0800

    test: test cases cover most scene

---
+++ test/config-1/test.pre.js
@@ -0,0 +1,12 @@
+module.exports = {
+  key: "value2",
+  deepFreeze: {
+    array: [ 1, '2', { key: 'value' } ],
+    num: 1,
+    string: 'string',
+    bool: true,
+    obj: {
+      key: 'value'
+    }
+  }
+}
---
+++ test/lib/ConfigParser.test.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const test = require("ava");
+const ConfigParser = require("../../lib/ConfigParser");
+
+test.afterEach(t => {
+  process.env.NODE_ENV = undefined;
+});
+
+test('load javascript config file', (t) => {
+  const paserdObj = ConfigParser.parse(process.cwd() + '/test/config-1/test.default.js', '');
+  t.is(paserdObj.key, 'value');
+});
+
+
+test('load json config file', (t) => {
+  const filepath = process.cwd() + '/test/config-2/test.default.json';
+  const jsonContent = fs.readFileSync(filepath)
+  const paserdObj = ConfigParser.parse(filepath, jsonContent);
+  t.is(paserdObj.key, 'default value');
+});
